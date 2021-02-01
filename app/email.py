1ac5cb411f27f4804e2dbcd7be0d8e19092be821
Matthias <matthiasaustin@gmail.com>
2021-02-01 08:41:55 -0800

    Added get message function

--- app/email.py
+++ app/email.py
@@ -2,14 +2,20 @@ import jinja2
 import os
 
 # make_message
-templateLoader = jinja2.FileSystemLoader(searchpath="../templates")
-templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "email.html"
-template = templateEnv.get_template(TEMPLATE_FILE)
-name = "Testing"
-outputText = template.render(name=name)  # this is where to put args to the template renderer
+def get_message():
+    templateLoader = jinja2.FileSystemLoader(searchpath="../templates")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "email.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    name = "Testing"
+    outputText = template.render(name=name)  # this is where to put args to the template renderer
 
-print(outputText)
+    return outputText
+
+def make_email():
 # import_address
 
 # import_message
+
+
+if __name__ == "__main__"
