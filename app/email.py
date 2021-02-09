d3bd9ea8dbda6c54b350cd4013eef813da16a4f7
Matthias <matthiasaustin@gmail.com>
2021-02-08 18:53:43 -0800

    email.py working on solo jobs.
    Next step is to incorporate it into the update process.

--- app/email.py
+++ app/email.py
@@ -6,9 +6,10 @@ import glob
 from datetime import datetime
 
 class Message():
+
     def __init__(self, df_row):
-        recipient = df_row[0]
         self.name = ""
+        self.recipient = df_row
         self.message_type = ""
         self.template = ""
         self.outputText = "None has been specified. Please run a constructor"
@@ -16,18 +17,20 @@ class Message():
         templateLoader = jinja2.FileSystemLoader(searchpath="../email_data/templates")
         templateEnv = jinja2.Environment(loader=templateLoader)
 
-    def cpr_start_email():
-        self.email = str(recipient['personal_email'])
-        self.supervisor_email = str(recipient['sup_email'])
+    def cpr_start_email(self):
+        self.email = str(self.recipient['personal_email'])
+        self.supervisor_email = str(self.recipient['sup_email'])
         self.attachement = None
-        self.name = str(recipient['first_name'])
-        self.link = str(recipient['registration_link'])
-        self.close_date = str(recipient['closing_date'])
+        self.name = str(self.recipient['first_name'])
+        self.link = str(self.recipient['registration_link'])
+        self.close_date = str(self.recipient['closing_date'])
         self.template_file = 'cpr_email.html'
         date = datetime.now()
         date = date.strftime("%m/%d/%Y")
         self.subject = f"CPR/First Aid Required - {date}"
-        template = templateEnv.get_template(template_file)
+        templateLoader = jinja2.FileSystemLoader(searchpath="../email_data/templates")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        template = templateEnv.get_template(self.template_file)
         name = self.name
         link = self.link
         close_date = self.close_date
@@ -35,48 +38,45 @@ class Message():
                                      link=link,
                                      close_date=close_date)
 
-    def fhr_start_email():
-        self.email = str(recipient['email'])
-        self.supervisor_email = str(recipient['profile_field_supervisor_email'])
+    def fhr_start_email(self):
+        self.email = str(self.recipient['email'])
+        self.supervisor_email = str(self.recipient['profile_field_supervisor_email'])
         self.attachment = PATH = os.path.abspath("../email_data/attachments/feb_2021_syllabus.pdf")
         self.template_file = 'welcome_40hr.html'
-        self.name = str(recipient['firstname'])
+        self.name = str(self.recipient['firstname'])
         self.month = "February"
-        self.subject = f"Welcome to the {month} 40hr Core"
-        self.username = str(recipient['username'])
-        self.password = str(recipient['password'])
-        self.template = templateEnv.get_template(template_file)
+        self.subject = f"Welcome to the {self.month} 40hr Core"
+        self.username = str(self.recipient['username'])
+        self.password = str(self.recipient['password'])
+        templateLoader = jinja2.FileSystemLoader(searchpath="../email_data/templates")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        self.template = templateEnv.get_template(self.template_file)
         name = self.name
         month = self.month
         username = self.username
         password = self.password
-        self.outputText = template.render(name=name,  # Include args for render
+        self.outputText = self.template.render(name=name,  # Include args for render
                                           month=month,
                                           username=username,
                                           password=password)
 
-    def fhr_reminder_email():
+    def fhr_reminder_email(self):
         fhr_start_email()
         date = datetime.now()
         date = date.strftime("%m/%d/%Y")
         self.subject = f"Training Reminder/Update - {date}"
         self.template_file = 'reminder_40hr.html'
-        self.template = templateEnv.get_template(template_file)
+        templateLoader = jinja2.FileSystemLoader(searchpath="../email_data/templates")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        self.template = templateEnv.get_template(self.template_file)
         self.outputText = template.render(name=name,  # Include args for render
                                           month=month)
-    def get_body():
+    def get_body(self):
         return self.outputText
 
 
-def get_message(recipient, message_type):
-    message_type = message_type
-    templateLoader = jinja2.FileSystemLoader(searchpath="../email_data/templates")
-    templateEnv = jinja2.Environment(loader=templateLoader)
-
-
 # Currently windows only, may try to make system agnostic at somepoint
-def make_email(recipient, subject, message_type):
-    subject = subject
+def make_email(recipient, message_type):
     message_type = message_type
     message = Message(recipient[1])
     outlook = win32.Dispatch('outlook.application')
@@ -92,9 +92,8 @@ def make_email(recipient, subject, message_type):
     mail.To = message.email
     mail.CC = message.supervisor_email
     text = message.get_body()
-    subject = str(subject)
 
-    mail.Subject = subject
+    mail.Subject = message.subject
     if message.attachment != None:
         mail.Attachments.Add(Source=message.attachment)
     mail.HtmlBody = text
@@ -113,21 +112,20 @@ def import_info():
     for f in files:
         print(f)
 
-    csv_name= input("What file to use? - Use the full name after the last \\ \n")
+    # csv_name= input("What file to use? - Use the full name after the last \\ \n")
     PATH = os.path.join(PATH, csv_name)
     print(PATH)
     default_input_message = """
     Which would message?\n
     cpr\n
-    fwelcome (40hr Welcome message)\n
+    fstart (40hr Welcome message)\n
     freminder (40hr Reminder message)\n
     """
 
     message_type = input(default_input_message)
     recipients = pd.read_csv(PATH)
-    subject = input("What is the message subject?\n")
-    for row in recipients.iterrows():
-        make_email(row, subject, message_type)
+    for recipient_row in recipients.iterrows():
+        make_email(recipient_row, message_type)
 
 
 if __name__ == "__main__":
